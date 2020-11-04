a0c909dfd5c7949fe31161669a9ef78eab2c234a
khlam <4841220+khlam@users.noreply.github.com>
2020-11-03 18:19:32 -0800

    test

---
+++ config.sh
@@ -0,0 +1,8 @@
+# curl -sSL https://git.io/JTbbu | bash
+# sudo pacman-mirrors --fasttrack 		
+# sudo pacman -Syu
+# sudo systemctl enable fstrim.timer
+
+sudo pacman -Rcns kate konversation kdeconnect --noconfirm
+
+sudo pacman -S keepassxc --noconfirm
