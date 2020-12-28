ea06e6526c5d9ca50fbdd4be493002991e9c471e
khlam <4841220+khlam@users.noreply.github.com>
2020-12-27 16:31:07 -0800

    gnome

--- gnome-config.sh
+++ gnome-config.sh
@@ -2,7 +2,9 @@
 sudo pacman-mirrors --country United_States
 sudo pacman -Syu --noconfirm
 
-sudo pacman -S keepassxc vlc vim firefox snapd --noconfirm
+sudo pacman -Rcns totem --noconfirm
+
+sudo pacman -S keepassxc vlc vim firefox snapd--noconfirm
 sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
 
