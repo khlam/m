d2db6b3e8fd9b2470e906bd60186dd040951de7c
khlam <4841220+khlam@users.noreply.github.com>
2020-12-27 16:14:35 -0800

    gnome

---
+++ gnaome-config.sh
@@ -0,0 +1,11 @@
+sudo pacman-mirrors --country United_States
+sudo pacman -Syu --noconfirm
+
+sudo pacman -Rcns gnome-extra --noconfirm
+
+sudo pacman -S keepassxc vlc vim firefox snapd--noconfirm
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install snap-store
+sudo snap install chromium
+sudo snap install sublime-text --classic
